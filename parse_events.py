2ef1a9b7d82c82db569d0b76f4d2a9e8f26a729b
Max Fresonke <max@fresonke.com>
2015-10-27 14:33:17 -0400

    init commit

---
+++ parse_events.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import dateutil.parser
+
+def convert_tstamp(df, col_index):
+	df[col_index] = pd.to_datetime(df.apply(lambda row: row[col_index], axis=1))
+	return
+
+data = pd.read_csv('events_train.csv')
+# basic cleaning
+data = data[pd.notnull(data['event_id'])]
+data = data[pd.notnull(data['event_description'])]
+data = data[pd.notnull(data['start_tstamp'])]
+data = data[pd.notnull(data['confirmed_tstamp'])]
+data = data[pd.notnull(data['created_tstamp'])]
+data = data[pd.notnull(data['closed_tstamp'])]
+data = data[pd.notnull(data['event_type'])]
+data = data[pd.notnull(data['event_subtype'])]
+data = data[pd.notnull(data['location'])]
+data = data[pd.notnull(data['latitude'])]
+data = data[pd.notnull(data['longitude'])]
+data = data[pd.notnull(data['number_of_responders'])]
+data = data[pd.notnull(data['lanes_affected'])]
+
+bound_cols = ['lat_lower', 'long_lower', 'lat_upper', 'long_upper', 'tstamp_begin', 'tstamp_end']
+bounds = pd.read_csv('prediction_trials.tsv', sep='\t', names=bound_cols)
+
+convert_tstamp(bounds, 'tstamp_begin')
+convert_tstamp(bounds, 'tstamp_end')
+
+convert_tstamp(data, 'created_tstamp')
+convert_tstamp(data, 'confirmed_tstamp')
+convert_tstamp(data, 'closed_tstamp')
+convert_tstamp(data, 'start_tstamp')
+
+print data['created_tstamp']
+print type(data['created_tstamp'][0])
