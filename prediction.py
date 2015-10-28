b9c98d573bdd66ee64123fa9ef6d6adca3715a1d
Max Fresonke <max@fresonke.com>
2015-10-27 21:49:00 -0400

    Significant Progress on Prediction Code

--- prediction.py
+++ prediction.py
@@ -3,20 +3,27 @@ import time
 import csv
 import statsmodels.formula.api as sm
 
-
-
-
 start_time = time.time()
-eventsInBox = pd.read_csv('data/out.tsv', sep='\t')
+eventsInBox = pd.read_csv('output/aggregated.tsv', sep='\t')
 
-print eventsInBox[:3]
+groupedEvents = eventsInBox.groupby(['eventType', 'month', 'xmin', 'ymin', 'xmax', 'ymax'])		#gives eventtype of a month in a bounding box
 
-eventsInBox.groupby('eventType', 'month', 'xmin', 'ymin', 'xmax', 'ymax')		#gives eventtype of a month in a bounding box
+for key, group in groupedEvents:
+	yearMin = group['year'].min()
+	yearMax = group['year'].max()
+	for x in range(yearMin, yearMax):
+		if len(group[group['year'] == x]) == 0:
+			temp = pd.DataFrame({'numEvents': [0], 'year': [x]})
+			group = group.append(temp)
 
-
-
-
-
+	result = sm.ols(formula="numEvents ~ year", data=group).fit()
+	print '\n=========================='
+	print 'KEY: '
+	print key
+	print 'RESULT: '
+	print result.params['Intercept']
+	print result.params['year']
+	print '=========================='
 
 
 
