c5c85f4dcc4d063ede3b430d907edc4362b41eb8
Max Fresonke <max@fresonke.com>
2015-11-04 16:39:08 -0500

    Updated Plot Code

--- plot.py
+++ plot.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+
+eventsInBox = pd.read_csv('output/aggregated_events.csv')
+groupedEvents = eventsInBox.groupby(['eventType', 'month', 'xmin', 'ymin', 'xmax', 'ymax'])
 
 
+for key, group in groupedEvents:
+	group.plot(kind='scatter',x='year',y='numEvents')
+	break
 
-ts = pd.Series(np.random.randn(10000), index=pd.date_range('1/1/2000', periods=10000))
-ts = ts.cumsum()
-ts.plot(kind = 'scatter')
 plt.show()
