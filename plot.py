4ed4af5ab29871c0eae68197f0de34c831ebf765
Datascience Course Student <datascience@datascience-amd64.defaultdomain>
2015-11-04 13:52:50 -0800

    XD

--- plot.py
+++ plot.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-eventsInBox = pd.read_csv('output/aggregated_events.csv')
+eventsInBox = pd.read_csv('output/reference_aggregated_events.csv')
 groupedEvents = eventsInBox.groupby(['eventType', 'month', 'xmin', 'ymin', 'xmax', 'ymax'])
 
-
+onlyPlot = 20
 for key, group in groupedEvents:
-	group.plot(kind='scatter',x='year',y='numEvents')
-	break
+	if onlyPlot == 0:
+		break
+
+	eventType = group['eventType'][group['eventType'].keys()[0]]
+	xmax = group['xmax'][group['xmax'].keys()[0]]
+	ymax = group['ymax'][group['ymax'].keys()[0]]
+	month = group['month'][group['month'].keys()[0]]
+	title = eventType + str(xmax) + str(ymax) + str(month)
+	if len(group) > 5 and month == 3:
+		onlyPlot -= 1
+		group.plot(kind='scatter',x='year',y='numEvents', title = title)
+	
 
 plt.show()
+
